fb79ef1c540f22c27e3ed8c786016a0b67324c76
audrnjs9530 <p45771799@gmail.com>
2026-03-29 21:54:45 +0900

    초기 세팅

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    is_artist = models.BooleanField(default=False)
+    nickname = models.CharField(blank=True, max_length=30)
+
+    def __str__(self):
+        return self.username
